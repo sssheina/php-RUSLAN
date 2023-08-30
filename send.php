30fbaf8a9099b6d0c4769f17b0bac7121b5ddcfd
Veta Sheina <108290460+sssheina@users.noreply.github.com>
2023-08-31 01:58:19 +0300

    Update send.php

--- send.php
+++ send.php
@@ -1,10 +1,12 @@
 // Файлы phpmailer
-require 'phpmailer/PHPMailer.php';
-require 'phpmailer/SMTP.php';
-require 'phpmailer/Exception.php';
+require './phpmailer/PHPMailer.php';
+require './phpmailer/SMTP.php';
+require './phpmailer/Exception.php';
 
 # проверка, что ошибки нет
-if (!error_get_last()) {
+$data['result'] = "success";
+$data['info'] = "Сообщение успешно отправлено!";
+<!--/*if (!error_get_last()) {-->
 
     // Переменные, которые отправляет пользователь
     <!-- $name = $_POST['name'] ;
@@ -14,8 +16,8 @@ if (!error_get_last()) {
     
     
     // Формирование самого письма
-    $title = "Заголовок письма";
-    $body = "111";
+    <!--$title = "Заголовок письма";-->
+    <!--$body = "111";-->
     <!-- $body = "
     <h2>Новое письмо</h2>
     <b>Имя:</b> $name<br>
@@ -24,25 +26,25 @@ if (!error_get_last()) {
     "; -->
     
     // Настройки PHPMailer
-    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    <!--$mail = new PHPMailer\PHPMailer\PHPMailer();-->
     
-    $mail->isSMTP();   
-    $mail->CharSet = "UTF-8";
-    $mail->SMTPAuth   = true;
+    <!--$mail->isSMTP();   -->
+    <!--$mail->CharSet = "UTF-8";-->
+    <!--$mail->SMTPAuth   = true;-->
     //$mail->SMTPDebug = 2;
-    $mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};
+    <!--$mail->Debugoutput = function($str, $level) {$GLOBALS['data']['debug'][] = $str;};-->
     
     // Настройки вашей почты
-    $mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты
-    $mail->Username   = '_solar_'; // Логин на почте
-    $mail->Password   = '16girGMRNF5fVvEPUkSL'; // Пароль на почте
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port       = 465;
-    $mail->setFrom('_solar_@list.ru', 'Veta'); // Адрес самой почты и имя отправителя
+    <!--$mail->Host       = 'smtp.mail.ru'; // SMTP сервера вашей почты-->
+    <!--$mail->Username   = '_solar_'; // Логин на почте-->
+    <!--$mail->Password   = '16girGMRNF5fVvEPUkSL'; // Пароль на почте-->
+    <!--$mail->SMTPSecure = 'ssl';-->
+    <!--$mail->Port       = 465;-->
+    <!--$mail->setFrom('_solar_@list.ru', 'Veta'); // Адрес самой почты и имя отправителя-->
     
     // Получатель письма
-    $mail->addAddress('sssheina@gmail.com');  
-    $mail->addAddress('sviatlana@mail.ru'); // Ещё один, если нужен
+    <!--$mail->addAddress('sssheina@gmail.com');  -->
+    <!--$mail->addAddress('sviatlana@mail.ru'); // Ещё один, если нужен-->
     
     // Прикрипление файлов к письму
     <!-- if (!empty($file['name'][0])) {
@@ -52,25 +54,25 @@ if (!error_get_last()) {
         }
     } -->
     // Отправка сообщения
-    $mail->isHTML(true);
-    $mail->Subject = $title;
-    $mail->Body = $body;    
+    <!--$mail->isHTML(true);-->
+    <!--$mail->Subject = $title;-->
+    <!--$mail->Body = $body;    -->
     
     // Проверяем отправленность сообщения
-    if ($mail->send()) {
-        $data['result'] = "success";
-        $data['info'] = "Сообщение успешно отправлено!";
-    } else {
-        $data['result'] = "error";
-        $data['info'] = "Сообщение не было отправлено. Ошибка при отправке письма";
-        $data['desc'] = "Причина ошибки: {$mail->ErrorInfo}";
-    }
+<!--    if ($mail->send()) {-->
+<!--        $data['result'] = "success";-->
+<!--        $data['info'] = "Сообщение успешно отправлено!";-->
+<!--    } else {-->
+<!--        $data['result'] = "error";-->
+<!--        $data['info'] = "Сообщение не было отправлено. Ошибка при отправке письма";-->
+<!--        $data['desc'] = "Причина ошибки: {$mail->ErrorInfo}";-->
+<!--    }-->
     
-} else {
-    $data['result'] = "error";
-    $data['info'] = "В коде присутствует ошибка";
-    $data['desc'] = error_get_last();
-}
+<!--} else {-->
+<!--    $data['result'] = "error";-->
+<!--    $data['info'] = "В коде присутствует ошибка";-->
+<!--    $data['desc'] = error_get_last();-->
+<!--}*/-->
 
 // Отправка результата
 header('Content-Type: application/json');
